c33bc0b8f3f6d492d12ef17cd267fd934645adf4
Chessman-12 <mornob211001@bscse.uiu.ac.bd>
2026-04-18 22:45:38 +0600

    working of authentication in the frontend

--- frontend/ai-learning-assistant/src/pages/Auth/LoginPage.jsx
+++ frontend/ai-learning-assistant/src/pages/Auth/LoginPage.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
+import authService from '../../services/authService';
+import { brainCircuit, Mail, Lock, ArrowRight} from 'lucide-react';
+import toast from 'react-hot-toast';
 
 const LoginPage = () => {
+
+    const [email, setEmail] = useState('alex@timetoprogram.com');
+    const [password, serPassword] = useState('Test@123')
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [focusedField, setFocusedField] = useState(null);
+
+    const
+
     return (
         <div>LoginPage</div>
     )
